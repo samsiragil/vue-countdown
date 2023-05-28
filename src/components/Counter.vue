c73d93ec76ef99f3bb38716cafcc2e70f6a3fa2c
ragil <samsiragil@gmail.com>
2023-05-29 00:47:27 +0800

    functional and basic style

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,9 +1,54 @@
 <script setup>
+  import {reactive,ref,onMounted} from 'vue'
+  import moment from 'moment'
 
+  const countdown = ref(moment())
+  const timeout = ref(false)
+  const deadline = ref(moment().set({'year': 2023, 'month': 11, 'date': 3, 'hour': 0, 'minute': 0, 'second': 0}))
+  const setupCounter = () =>{
+    let now = moment()
+    countdown.value = moment.duration(moment(deadline.value).diff(moment(now)))
+    let looping = setInterval(() => {
+      countdown.value = countdown.value.subtract(1, 'seconds')
+      if (countdown.value.months() <= 0 && countdown.value.weeks() <= 0 && countdown.value.hours() <= 0 && countdown.value.minutes() <= 0 && countdown.value.seconds() <= 0) {
+        clearInterval(looping)
+        timeout.value = true
+      }
+    },1000)
+  }
+
+  const constFormatNumber = (data) => {
+    return data < 10 ? `0${data}` : data
+  }
+
+  onMounted(()=>{
+    setupCounter()
+  })
 </script>
 
 <template>
-  <div>
-    <h3>Hie from counter components</h3>
+  <div class="container-fluid">
+    <div class="w-100 no-ws">
+      <p class="mb-0 counter-item timer time">{{ constFormatNumber(countdown.months()) }}</p>
+      <p class="mb-0 counter-item timer separator">:</p>
+      <p class="mb-0 counter-item timer time">{{ constFormatNumber(countdown.days()) }}</p>
+      <p class="mb-0 counter-item timer separator">:</p>
+      <p class="mb-0 counter-item timer time">{{ constFormatNumber(countdown.hours()) }}</p>
+      <p class="mb-0 counter-item timer separator">:</p>
+      <p class="mb-0 counter-item timer time">{{ constFormatNumber(countdown.minutes()) }}</p>
+      <p class="mb-0 counter-item timer separator">:</p>
+      <p class="mb-0 counter-item timer time">{{ constFormatNumber(countdown.seconds()) }}</p>
+    </div>
+    <div class="w-100 no-ws mb-3">
+      <p class="mb-0 counter-item label time">MONTHS</p>
+      <p class="mb-0 counter-item label separator"></p>
+      <p class="mb-0 counter-item label time">DAYS</p>
+      <p class="mb-0 counter-item label separator"></p>
+      <p class="mb-0 counter-item label time">HOURS</p>
+      <p class="mb-0 counter-item label separator"></p>
+      <p class="mb-0 counter-item label time">MINUTES</p>
+      <p class="mb-0 counter-item label separator"></p>
+      <p class="mb-0 counter-item label time">SECONDS</p>
+    </div>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,15 @@ import Counter from '../components/Counter.vue';
 </script>
 
 <template>
-  <div class="w-100 text-center py-4">
-    <h1>Hello World</h1>
-    <Counter />
+  <div class="container text-center" style="margin-top: 15%;">
+    <div class="row">
+      <div class="col-7 align-self-center">
+        <h1 class="text-bold text-blue mb-4 text-big" style="letter-spacing: 16px;">RAGIL'S BIRTHDAY</h1>
+        <Counter />
+      </div>
+      <div class="col-5 align-self-center">
+        <img src="../assets/party.png" class="img-fluid" />
+      </div>
+    </div>
   </div>
 </template>
