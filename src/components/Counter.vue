2a617d92a319426764d1f2893916efcba07dd7bb
ragil <samsiragil@gmail.com>
2023-05-28 23:13:25 +0800

    Init Project

---
+++ src/components/Counter.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h3>Hie from counter components</h3>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Counter from '../components/Counter.vue';
+</script>
+
+<template>
+  <div class="w-100 text-center py-4">
+    <h1>Hello World</h1>
+    <Counter />
+  </div>
+</template>
